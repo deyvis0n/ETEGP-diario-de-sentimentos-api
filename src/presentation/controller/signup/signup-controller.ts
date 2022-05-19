4a8e9422e93c6f3c6f4d93da52ebce73550fb64d
deyvis0n <deyvisonsferreira@outlook.com>
2022-05-19 14:50:10 -0300

    feat: ensure EmailValidator calls with correct values

--- src/presentation/controller/signup/signup-controller.ts
+++ src/presentation/controller/signup/signup-controller.ts
@@ -3,8 +3,13 @@ import { Controller } from '../../protocols/signup'
 import { MissingParamError } from '../../erros/missing-param-error'
 import { badRequest } from '../../helper/http/http-helper'
 import { InvalidParamError } from '../../erros/invalid-param-error'
+import { EmailValidator } from '../../../validation/protocols/email-validator'
 
 export class SignUpController implements Controller {
+  constructor (
+    private readonly emailValidator: EmailValidator
+  ) {}
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
@@ -12,9 +17,10 @@ export class SignUpController implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
-    const { password, passwordConfirmation } = httpRequest.body
+    const { email, password, passwordConfirmation } = httpRequest.body
     if (password !== passwordConfirmation) {
       return badRequest(new InvalidParamError(passwordConfirmation))
     }
+    this.emailValidator.isValid(email)
   }
 }

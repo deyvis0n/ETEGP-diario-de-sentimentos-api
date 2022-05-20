e92ba6e667f3a449f51afab940fd1d417231c079
deyvis0n <deyvisonsferreira@outlook.com>
2022-05-20 15:23:41 -0300

    feat: ensure isValid returns true if valid emails is provided

--- src/infra/validators/email-validator.ts
+++ src/infra/validators/email-validator.ts
@@ -2,6 +2,7 @@ import { EmailValidator } from '../../validation/protocols/email-validator'
 
 export class EmailValidatorAdapter implements EmailValidator {
   isValid (email: string): boolean {
-    return false
+    const regex: RegExp = /^[a-zA-Z]{3,20}.[a-zA-Z]{3,20}@aluno.educacao.pe.gov.br.com$/
+    return regex.test(email)
   }
 }

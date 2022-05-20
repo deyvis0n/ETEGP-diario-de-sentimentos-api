db731d1136500a0354908ee8b6500b4c554e2040
deyvis0n <deyvisonsferreira@outlook.com>
2022-05-20 16:05:22 -0300

    test: ensure DbAddAccount calls Hasher with correct value

---
+++ src/data/usecases/db-add-account.spec.ts
@@ -0,0 +1,41 @@
+import { AddAccountModel } from '../../domain/usercase/add-account'
+import { Hasher } from '../protocols/criptography/hasher'
+import { DbAddAccount } from './db-add-account'
+
+const makeHasher = (): Hasher => {
+  class HasherStub implements Hasher {
+    async hash (value: string): Promise<string> {
+      return 'hashed_password'
+    }
+  }
+  return new HasherStub()
+}
+
+const makeFakeAddAccount = (): AddAccountModel => ({
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'valid_password'
+})
+
+interface SutTypes {
+  sut: DbAddAccount
+  hasherStub: Hasher
+}
+
+const makeSut = (): SutTypes => {
+  const hasherStub = makeHasher()
+  const sut = new DbAddAccount(hasherStub)
+  return {
+    hasherStub,
+    sut
+  }
+}
+
+describe('DbAddAccount', () => {
+  test('Should call Hasher with correct value', async () => {
+    const { sut, hasherStub } = makeSut()
+    const hasherSpy = jest.spyOn(hasherStub, 'hash')
+    await sut.add(makeFakeAddAccount())
+    expect(hasherSpy).toBeCalledWith('valid_password')
+  })
+})

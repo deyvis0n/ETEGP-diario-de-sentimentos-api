90c2fe06eb15d2db6ac7203ddf18ae7b8538499b
deyvis0n <deyvisonsferreira@outlook.com>
2022-05-23 11:38:42 -0300

    refactor: move BcrypterAdapter to a factory method

--- src/infra/criptography/bcrypt-adapter/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter/bcrypt-adapter.spec.ts
@@ -7,16 +7,21 @@ jest.mock('bcrypt', () => ({
   }
 }))
 
+const salt = 12
+const makeSut = (): BcrypterAdapter => {
+  return new BcrypterAdapter(salt)
+}
+
 describe('BcryptAdapter', () => {
   test('Should call hash with correct values', async () => {
-    const sut = new BcrypterAdapter()
+    const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.hash('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', 12)
   })
 
   test('Should return a valid hash on hash success', async () => {
-    const sut = new BcrypterAdapter()
+    const sut = makeSut()
     const hash = await sut.hash('any_value')
     expect(hash).toBe('hash')
   })

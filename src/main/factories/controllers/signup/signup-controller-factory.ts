86f0f248ff1edf5462bf8860683b5327ddd1e44a
deyvis0n <deyvisonsferreira@outlook.com>
2022-05-23 20:28:59 -0300

    feat: add passwordValidator on SignUpController factory

--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -2,6 +2,7 @@ import { Controller } from '../../../../presentation/protocols/signup'
 import { SignUpController } from '../../../../presentation/controller/signup/signup-controller'
 import { DbAddAccount } from '../../../../data/usecases/db-add-account'
 import { EmailValidatorAdapter } from '../../../../infra/validators/email-validator'
+import { PasswordValidatorAdapter } from '../../../../infra/validators/password-validator-adapter'
 import { BcrypterAdapter } from '../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository'
 
@@ -11,6 +12,7 @@ export const makeSignUpController = (): Controller => {
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(hash, accountMongoRepository)
   const emailValidatorAdapter = new EmailValidatorAdapter()
-  const controller = new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const passwordValidatorAdapter = new PasswordValidatorAdapter()
+  const controller = new SignUpController(emailValidatorAdapter, dbAddAccount, passwordValidatorAdapter)
   return controller
 }

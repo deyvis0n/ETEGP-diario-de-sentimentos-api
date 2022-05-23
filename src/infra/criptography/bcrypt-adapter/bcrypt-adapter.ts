d360106dc333bba7421f535405f900173a9efd9a
deyvis0n <deyvisonsferreira@outlook.com>
2022-05-23 11:35:38 -0300

    refactor: move hash salt to a constructor

--- src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -2,8 +2,10 @@ import { Hasher } from '../../../data/protocols/criptography/hasher'
 import bcrypt from 'bcrypt'
 
 export class BcrypterAdapter implements Hasher {
+  constructor (private readonly salt: number) {}
+
   async hash (value: string): Promise<string> {
-    const hash = await bcrypt.hash(value, 12)
+    const hash = await bcrypt.hash(value, this.salt)
     return hash
   }
 }

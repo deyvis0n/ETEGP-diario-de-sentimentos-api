2081f505a5b68d575d66e21c7f3fe31c8ca2a75f
deyvis0n <deyvisonsferreira@outlook.com>
2022-05-23 15:46:34 -0300

    feat: add adaptRoute

---
+++ src/main/adapter/express/express-router-adapter.ts
@@ -0,0 +1,19 @@
+import { HttpRequest } from '../../../presentation/protocols/http'
+import { Controller } from '../../../presentation/protocols/signup'
+import { Request, Response } from 'express'
+
+export const adaptRoute = (controller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+    const httpReponse = await controller.handle(httpRequest)
+    if (httpReponse.statusCode === 200) {
+      res.status(httpReponse.statusCode).json(httpReponse.body)
+    } else {
+      res.status(httpReponse.statusCode).json({
+        error: httpReponse.body.message
+      })
+    }
+  }
+}

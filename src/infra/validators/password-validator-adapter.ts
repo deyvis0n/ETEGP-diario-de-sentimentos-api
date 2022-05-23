07ca9fd795f5d894d2a5c7e79246e97bdd2ef860
deyvis0n <deyvisonsferreira@outlook.com>
2022-05-23 19:12:00 -0300

    refactor: rename PassWordValidator to PasswordValidator

--- src/infra/validators/password-validator-adapter.ts
+++ src/infra/validators/password-validator-adapter.ts
@@ -1,6 +1,6 @@
-import { PassWordValidator } from '../../validation/protocols/password-validator'
+import { PasswordValidator } from '../../validation/protocols/password-validator'
 
-export class PasswordValidatorAdapter implements PassWordValidator {
+export class PasswordValidatorAdapter implements PasswordValidator {
   isValid (password: string): boolean {
     const regex: RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%&*.?;,+-= ]).{8,16}$/
     return regex.test(password)
--- src/validation/protocols/password-validator.ts
+++ src/validation/protocols/password-validator.ts
@@ -1,3 +1,3 @@
-export interface PassWordValidator {
+export interface PasswordValidator {
   isValid: (password: string) => boolean
 }

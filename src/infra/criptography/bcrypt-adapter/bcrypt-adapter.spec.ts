34f968f01d523f1ad5ed0dd0fcf45fb118cf2a3d
deyvis0n <deyvisonsferreira@outlook.com>
2022-05-23 11:31:02 -0300

    test: ensure BcryptAdapter return a valid hash if hash success

--- src/infra/criptography/bcrypt-adapter/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter/bcrypt-adapter.spec.ts
@@ -1,11 +1,23 @@
 import bcrypt from 'bcrypt'
 import { BcrypterAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return 'hash'
+  }
+}))
+
 describe('BcryptAdapter', () => {
   test('Should call hash with correct values', async () => {
     const sut = new BcrypterAdapter()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.hash('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', 12)
+  })
+
+  test('Should return a valid hash on hash success', async () => {
+    const sut = new BcrypterAdapter()
+    const hash = await sut.hash('any_value')
+    expect(hash).toBe('hash')
   })
 })

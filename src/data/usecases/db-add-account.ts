52194a8116099c887c9e0a05cbc6d4e729d29ca3
deyvis0n <deyvisonsferreira@outlook.com>
2022-05-23 11:13:11 -0300

    feat: ensure DbAddAccount return account on success

--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -11,7 +11,7 @@ export class DbAddAccount implements AddAccount {
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.hasher.hash(accountData.password)
-    await this.addAccountRepository.add(Object.assign(accountData, { password: hashedPassword }))
-    return null
+    const account = await this.addAccountRepository.add(Object.assign(accountData, { password: hashedPassword }))
+    return account
   }
 }

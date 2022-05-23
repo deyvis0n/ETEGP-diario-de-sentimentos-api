3b06a8cd422e42e2c34953db774511abb2ec8636
deyvis0n <deyvisonsferreira@outlook.com>
2022-05-23 11:03:43 -0300

    fix: email regex

--- src/infra/validators/email-validator.ts
+++ src/infra/validators/email-validator.ts
@@ -2,7 +2,7 @@ import { EmailValidator } from '../../validation/protocols/email-validator'
 
 export class EmailValidatorAdapter implements EmailValidator {
   isValid (email: string): boolean {
-    const regex: RegExp = /^[a-zA-Z]{3,20}.[a-zA-Z]{3,20}@aluno.educacao.pe.gov.br.com$/
+    const regex: RegExp = /^[a-zA-Z]{3,20}\.[a-zA-Z]{3,20}@aluno.educacao.pe.gov.br.com$/
     return regex.test(email)
   }
 }

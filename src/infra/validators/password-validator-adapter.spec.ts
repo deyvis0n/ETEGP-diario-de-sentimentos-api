c58376958ac57059b0f2b2bf6e2e794f1054abeb
deyvis0n <deyvisonsferreira@outlook.com>
2022-05-23 19:04:40 -0300

    test: ensure PasswordValidatorAdapter returns false if invalid password is provided

--- src/infra/validators/password-validator-adapter.spec.ts
+++ src/infra/validators/password-validator-adapter.spec.ts
@@ -6,4 +6,10 @@ describe('PasswordValidatorAdapter', () => {
     const isValid = sut.isValid('Aa@12345')
     expect(isValid).toBe(true)
   })
+
+  test('Should return false if a invalid password is provided', () => {
+    const sut = new PasswordValidatorAdapter()
+    const isValid = sut.isValid('12345678')
+    expect(isValid).toBe(false)
+  })
 })

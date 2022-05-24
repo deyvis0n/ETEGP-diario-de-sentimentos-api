e3064fe1121b1b0814572cdb1bb8bd3d03eabbbb
deyvis0n <deyvisonsferreira@outlook.com>
2022-05-24 12:30:51 -0300

    feat: ensure LoginController return 400 if no name is provided

---
+++ src/presentation/controller/login/login-controller.ts
@@ -0,0 +1,16 @@
+import { MissingParamError } from '../../erros/missing-param-error'
+import { badRequest } from '../../helper/http/http-helper'
+import { HttpRequest, HttpResponse } from '../../protocols/http'
+import { Controller } from '../../protocols/signup'
+
+export class LoginController implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const requiredFields = ['name']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
+    }
+    return null
+  }
+}

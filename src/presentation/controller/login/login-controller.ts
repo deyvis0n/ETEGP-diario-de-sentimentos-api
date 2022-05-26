1b5f8019d6d0e09e743938b3cf14b0f18b5002a4
deyvis0n <deyvisonsferreira@outlook.com>
2022-05-26 15:33:19 -0300

    feat: ensure LoginController calls Authentication with correct values

--- src/presentation/controller/login/login-controller.ts
+++ src/presentation/controller/login/login-controller.ts
@@ -1,9 +1,12 @@
+import { Authentication } from '../../../domain/usercase/authentication'
 import { MissingParamError } from '../../erros/missing-param-error'
 import { badRequest } from '../../helper/http/http-helper'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 import { Controller } from '../../protocols/signup'
 
 export class LoginController implements Controller {
+  constructor (private readonly authentication: Authentication) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ['email', 'password']
     for (const field of requiredFields) {
@@ -11,6 +14,8 @@ export class LoginController implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
+    const { email, password } = httpRequest.body
+    await this.authentication.auth({ email, password })
     return null
   }
 }

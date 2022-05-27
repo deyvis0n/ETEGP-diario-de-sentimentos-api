b918dc8afd4b94e2dd74fa5f31efd65b2632b99d
deyvis0n <deyvisonsferreira@outlook.com>
2022-05-27 16:44:00 -0300

    test: ensure AccountMongoRepository return account if loadByEmail success

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -6,6 +6,10 @@ describe('AccountMongoRepository', () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
 
+  beforeEach(async () => {
+    await MongoHelper.getCollection('accounts').deleteMany({})
+  })
+
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
@@ -17,6 +21,21 @@ describe('AccountMongoRepository', () => {
       email: 'any_email@mail.com',
       password: 'any_password'
     })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+
+  test('Should calls findOne with correct values', async () => {
+    const sut = new AccountMongoRepository()
+    await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    const account = await sut.loadByEmail('any_email@mail.com')
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe('any_name')

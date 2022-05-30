e43b8c54de52d52c5e0250473028584f99b4426f
deyvis0n <deyvisonsferreira@outlook.com>
2022-05-30 17:00:14 -0300

    fix: add emailValidator dependence on makeLoginController

--- src/main/factories/controllers/login/login-controller-factory.ts
+++ src/main/factories/controllers/login/login-controller-factory.ts
@@ -1,9 +1,11 @@
 import { Controller } from '../../../../presentation/protocols/signup'
 import { LoginController } from '../../../../presentation/controller/login/login-controller'
 import { makeDbAuthentication } from '../../usecases/authentication-factory'
+import { EmailValidatorAdapter } from '../../../../infra/validators/email-validator'
 
 export const makeLoginController = (): Controller => {
   const dbAuthentication = makeDbAuthentication()
-  const controller = new LoginController(dbAuthentication)
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const controller = new LoginController(dbAuthentication, emailValidatorAdapter)
   return controller
 }

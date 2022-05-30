fd5efde89515b386d60889e7d7b6214ce1ff6b1a
deyvis0n <deyvisonsferreira@outlook.com>
2022-05-29 23:27:25 -0300

    fix: password regex

--- src/infra/validators/password-validator-adapter.ts
+++ src/infra/validators/password-validator-adapter.ts
@@ -2,7 +2,7 @@ import { PasswordValidator } from '../../validation/protocols/password-validator
 
 export class PasswordValidatorAdapter implements PasswordValidator {
   isValid (password: string): boolean {
-    const regex: RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%&*.?;,+-= ]).{8,16}$/
+    const regex: RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%&*.?;, ]).{8,16}$/
     return regex.test(password)
   }
 }

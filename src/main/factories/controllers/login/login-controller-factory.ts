bd0cc3da9e8e91d6cafab7962c6ed850f4af182e
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-01 16:46:22 -0300

    refactor: remove emailValidator on makeLoginController factory

--- src/main/factories/controllers/login/login-controller-factory.ts
+++ src/main/factories/controllers/login/login-controller-factory.ts
@@ -1,12 +1,10 @@
 import { Controller } from '../../../../presentation/protocols/signup'
 import { LoginController } from '../../../../presentation/controller/login/login-controller'
 import { makeDbAuthentication } from '../../usecases/authentication-factory'
-import { EmailValidatorAdapter } from '../../../../infra/validators/email-validator'
 import { makeLoginValidation } from './login-validation-factory'
 
 export const makeLoginController = (): Controller => {
   const dbAuthentication = makeDbAuthentication()
-  const emailValidatorAdapter = new EmailValidatorAdapter()
-  const controller = new LoginController(dbAuthentication, emailValidatorAdapter, makeLoginValidation())
+  const controller = new LoginController(dbAuthentication, makeLoginValidation())
   return controller
 }

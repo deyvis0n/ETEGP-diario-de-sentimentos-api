ff143658534045983bfc31b42ba2899acc717c2d
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-01 13:23:30 -0300

    test: ensure ValidationComposite returns first error if more than one validation return error

--- src/validation/validator/validation.composite.spec.ts
+++ src/validation/validator/validation.composite.spec.ts
@@ -1,3 +1,4 @@
+import { MissingParamError } from '../../presentation/erros/missing-param-error'
 import { Validation } from '../../presentation/protocols/validation'
 import { ValidationComposite } from './validation.composite'
 
@@ -28,10 +29,18 @@ const makeSut = (): sutTypes => {
 }
 
 describe('ValidationComposite', () => {
-  test('Should return error if any validation return error', () => {
+  test('Should return error if any validation return errors', () => {
     const { sut, validationStub } = makeSut()
     jest.spyOn(validationStub[0], 'validate').mockReturnValueOnce(new Error())
     const error = sut.validate({ field: 'any_field ' })
+    expect(error).toEqual(Error())
+  })
+
+  test('Should return the first error if more than one validation return error', () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub[0], 'validate').mockReturnValueOnce(new Error())
+    jest.spyOn(validationStub[1], 'validate').mockReturnValueOnce(new MissingParamError('field'))
+    const error = sut.validate({ invalidField: 'any_field' })
     expect(error).toEqual(Error())
   })
 })

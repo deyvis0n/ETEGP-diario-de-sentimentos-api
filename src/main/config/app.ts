414c7d4634b8b94dc05c7f171e03d90c8dc08ff8
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-01 18:27:58 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import setupMiddleware from './middlewares'
+import setubSwagger from './swagger'
 import setupRoutes from './router'
 
 const app = express()
+setubSwagger(app)
 setupMiddleware(app)
 setupRoutes(app)
 export default app
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Diario de Sentimentos API',
+    description: 'API para projeto da ETEGP',
+    version: '0.0.0'
+  }
+}

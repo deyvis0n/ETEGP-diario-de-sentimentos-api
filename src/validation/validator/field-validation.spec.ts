e51f96d14929c0aa9413e6d6490698a48df522ab
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-01 16:10:11 -0300

    test: ensure FieldValidation return InvalidParamError if FieldValidator return false

--- src/validation/validator/field-validation.spec.ts
+++ src/validation/validator/field-validation.spec.ts
@@ -1,5 +1,6 @@
 import { FieldValidator } from '../protocols/field-validator'
 import { FieldValidation } from './field-validation'
+import { InvalidParamError } from '../../presentation/erros/invalid-param-error'
 
 const makeFieldValidator = (): FieldValidator => {
   class FieldValidatorStub implements FieldValidator {
@@ -30,5 +31,12 @@ describe('FieldValidation', () => {
     const isValidSpy = jest.spyOn(fieldValidatorStub, 'isValid')
     sut.validate({ field: 'any_field' })
     expect(isValidSpy).toBeCalledWith('any_field')
+  })
+
+  test('Should return an Error if FieldValidator return an Error', () => {
+    const { sut, fieldValidatorStub } = makeSut()
+    jest.spyOn(fieldValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const error = sut.validate({ invalidfield: 'any_field' })
+    expect(error).toEqual(new InvalidParamError('field'))
   })
 })

e85f8b51bcdd42909e490a319108c455ed995d5a
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-01 13:00:00 -0300

    test: ensure RequiredFieldValidation return an Error if required field is not provided

---
+++ src/validation/validator/requeried-field-validation.spec.ts
@@ -0,0 +1,10 @@
+import { MissingParamError } from '../../presentation/erros/missing-param-error'
+import { RequiredFieldValidation } from './requeried-field-validation'
+
+describe('RequiredFieldValidation', () => {
+  test('Should return an Error if required field is not provided', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ invalidField: 'field' })
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+})

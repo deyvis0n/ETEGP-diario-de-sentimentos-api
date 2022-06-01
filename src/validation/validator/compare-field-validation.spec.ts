caf906d8b6310514667d74b4fa577369f0f9d343
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-01 17:16:48 -0300

    test: ensure CompareFieldValidation return null if validate success

--- src/validation/validator/compare-field-validation.spec.ts
+++ src/validation/validator/compare-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe('CompareFieldValidation', () => {
     const error = sut.validate({ field: 'valid_field', compareField: 'invalid_field' })
     expect(error).toEqual(new InvalidParamError('compareField'))
   })
+
+  test('Should return null if validate success', () => {
+    const sut = new CompareFieldValidation('field', 'compareField')
+    const error = sut.validate({ field: 'valid_field', compareField: 'valid_field' })
+    expect(error).toBeNull()
+  })
 })

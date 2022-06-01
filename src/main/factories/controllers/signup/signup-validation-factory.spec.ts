e8634a69850b636c7765371e63fe7a045f5c49dc
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-01 16:58:52 -0300

    feat: ensure makeLoginValidation calls Validations with correct values

---
+++ src/main/factories/controllers/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,22 @@
+import { makeSignUpController } from './signup-controller-factory'
+import { ValidationComposite } from '../../../../validation/validator/validation.composite'
+import { Validation } from '../../../../presentation/protocols/validation'
+import { RequiredFieldValidation } from '../../../../validation/validator/requeried-field-validation'
+import { FieldValidation } from '../../../../validation/validator/field-validation'
+import { EmailValidatorAdapter } from '../../../../infra/validators/email-validator'
+import { PasswordValidatorAdapter } from '../../../../infra/validators/password-validator-adapter'
+
+jest.mock('../../../../validation/validator/validation.composite')
+
+describe('makeLoginValidation', () => {
+  test('Should calls validationComposite with correct values', () => {
+    makeSignUpController()
+    const validations: Validation[] = []
+    for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    validations.push(new FieldValidation('email', new EmailValidatorAdapter()))
+    validations.push(new FieldValidation('password', new PasswordValidatorAdapter()))
+    expect(ValidationComposite).toBeCalledWith(validations)
+  })
+})
---
+++ src/main/factories/controllers/signup/signup-validation-factory.ts
@@ -0,0 +1,16 @@
+import { ValidationComposite } from '../../../../validation/validator/validation.composite'
+import { Validation } from '../../../../presentation/protocols/validation'
+import { RequiredFieldValidation } from '../../../../validation/validator/requeried-field-validation'
+import { FieldValidation } from '../../../../validation/validator/field-validation'
+import { EmailValidatorAdapter } from '../../../../infra/validators/email-validator'
+import { PasswordValidatorAdapter } from '../../../../infra/validators/password-validator-adapter'
+
+export const makeLoginValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  validations.push(new FieldValidation('email', new EmailValidatorAdapter()))
+  validations.push(new FieldValidation('password', new PasswordValidatorAdapter()))
+  return new ValidationComposite(validations)
+}

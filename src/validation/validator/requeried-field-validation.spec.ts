09269ccb8d43d92d7179466f1ef8737b76cab980
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-01 13:01:59 -0300

    test: ensure RequiredFieldValidation return null if required field is provided

--- src/validation/validator/requeried-field-validation.spec.ts
+++ src/validation/validator/requeried-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe('RequiredFieldValidation', () => {
     const error = sut.validate({ invalidField: 'field' })
     expect(error).toEqual(new MissingParamError('field'))
   })
+
+  test('Should return null if required field is provided', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ field: 'field' })
+    expect(error).toBeNull()
+  })
 })

b217b0f48009c01a9eecd45cb16b5c54740a99e6
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-02 18:50:26 -0300

    feat: ensure AddUserPostController calls Validation with correct values

--- src/presentation/controller/user/add-user-post-controller.ts
+++ src/presentation/controller/user/add-user-post-controller.ts
@@ -1,11 +1,16 @@
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 import { AddUserPost } from '../../../domain/usercase/add-user-post'
+import { Validation } from '../../protocols/validation'
 
 export class AddUserPostController implements Controller {
-  constructor (private readonly addUserPost: AddUserPost) {}
+  constructor (
+    private readonly addUserPost: AddUserPost,
+    private readonly validation: Validation
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    this.validation.validate(httpRequest.body)
     await this.addUserPost.add(httpRequest.body)
     return null
   }

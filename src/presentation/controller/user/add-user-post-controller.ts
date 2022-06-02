8ae0e57e11f4be3b5a3bef69dfaa70337351aef0
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-02 19:02:27 -0300

    feat: ensure AddUserPostController throws if AddUserPost throws

--- src/presentation/controller/user/add-user-post-controller.ts
+++ src/presentation/controller/user/add-user-post-controller.ts
@@ -2,7 +2,7 @@ import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 import { AddUserPost } from '../../../domain/usercase/add-user-post'
 import { Validation } from '../../protocols/validation'
-import { badRequest } from '../../helper/http/http-helper'
+import { badRequest, serverError } from '../../helper/http/http-helper'
 
 export class AddUserPostController implements Controller {
   constructor (
@@ -11,11 +11,15 @@ export class AddUserPostController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+      await this.addUserPost.add(httpRequest.body)
+      return null
+    } catch (error) {
+      return serverError()
     }
-    await this.addUserPost.add(httpRequest.body)
-    return null
   }
 }

16d9f06888ee59852ca69e06981d7bb030251170
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-03 15:42:19 -0300

    feat: create UserPostController factory method

---
+++ src/main/factories/controllers/user/add-user-post-controller-factory.ts
@@ -0,0 +1,12 @@
+import { Controller } from '../../../../presentation/protocols/controller'
+import { AddUserPostController } from '../../../../presentation/controller/user/add-user-post-controller'
+import { makeAddUserPostValidation } from './add-user-post-validation-factory'
+import { DbAddAccountUserPost } from '../../../../data/usecases/add-user-post/db-add-user-post'
+import { UserPostMongoRepository } from '../../../../infra/db/mongodb/user/user-post-mongo-repository'
+
+export const makeUserPostControllerFactory = (): Controller => {
+  const userPostMongoRepository = new UserPostMongoRepository()
+  const dbAddAccountUserPost = new DbAddAccountUserPost(userPostMongoRepository)
+  const controller = new AddUserPostController(dbAddAccountUserPost, makeAddUserPostValidation())
+  return controller
+}

d8bf2e0d9eb614ca26818201818ce0692995641a
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-03 20:49:23 -0300

    test: ensure UserPostMongoRepository.findByUserId returns correct values

--- src/infra/db/mongodb/user/user-post-mongo-repository.spec.ts
+++ src/infra/db/mongodb/user/user-post-mongo-repository.spec.ts
@@ -31,4 +31,36 @@ describe('UserPostMongoRepository', () => {
     expect(collection.message).toEqual(userPost.message)
     expect(collection.date).toBeTruthy()
   })
+
+  test('Should findByUserId returns correct values', async () => {
+    const sut = new UserPostMongoRepository()
+    const targetId = MongoHelper.objectId('62981df76d3661cf33982509')
+    const noTargetId = MongoHelper.objectId('6297e8eb0bf54e0163efcc5f')
+    await userPostCollection.insertMany([
+      {
+        userId: targetId,
+        message: 'any_message_1',
+        date: new Date(2022, 1, 1)
+      },
+      {
+        userId: noTargetId,
+        message: 'any_message_2',
+        date: new Date(2022, 1, 1)
+      },
+      {
+        userId: targetId,
+        message: 'any_message_3',
+        date: new Date(2022, 1, 1)
+      }
+    ])
+    const userPostArray = await sut.findByUserId('62981df76d3661cf33982509')
+    expect(userPostArray[0].id).toBeTruthy()
+    expect(userPostArray[0].userId).toEqual('62981df76d3661cf33982509')
+    expect(userPostArray[0].message).toEqual('any_message_1')
+    expect(userPostArray[0].date).toBeTruthy()
+    expect(userPostArray[1].id).toBeTruthy()
+    expect(userPostArray[1].userId).toEqual('62981df76d3661cf33982509')
+    expect(userPostArray[1].message).toEqual('any_message_3')
+    expect(userPostArray[1].date).toBeTruthy()
+  })
 })

dbde92f83ab87f73da1d5064f656585d6008140c
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-03 12:59:12 -0300

    test: ensure DbAddAccountUserPost calls AddUserPostRepository with correct values

---
+++ src/data/usecases/add-user-post/db-add-user-post.spec.ts
@@ -0,0 +1,37 @@
+import { AddUserPostRepository } from '../../protocols/db/add-user-post-repository'
+import { DbAddAccountUserPost } from './db-add-user-post'
+import { AddUserPostModel } from '../../../domain/usercase/add-user-post'
+
+const makeAddUserPostRepository = (): AddUserPostRepository => {
+  class AddUserPostRepositoryStub implements AddUserPostRepository {
+    async add (userPost: AddUserPostModel): Promise<void> {}
+  }
+  return new AddUserPostRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbAddAccountUserPost
+  AddUserPostRepositoryStub: AddUserPostRepository
+}
+
+const makeSut = (): SutTypes => {
+  const AddUserPostRepositoryStub = makeAddUserPostRepository()
+  const sut = new DbAddAccountUserPost(AddUserPostRepositoryStub)
+  return {
+    sut,
+    AddUserPostRepositoryStub
+  }
+}
+
+describe('DbAddUserPost', () => {
+  test('Should call LoaAddUserPostRepository with correct values', async () => {
+    const { sut, AddUserPostRepositoryStub } = makeSut()
+    const addSpy = jest.spyOn(AddUserPostRepositoryStub, 'add')
+    const userPost = ({
+      userId: 'any_id',
+      message: 'any_message'
+    })
+    await sut.add(userPost)
+    expect(addSpy).toBeCalledWith(userPost)
+  })
+})

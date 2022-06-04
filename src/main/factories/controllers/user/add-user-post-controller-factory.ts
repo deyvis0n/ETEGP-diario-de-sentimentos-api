3dfaf4a32a2bd94b48567854156d95586bf15012
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-03 21:10:13 -0300

    refactor: remove Factory on makeUserPostController name

--- src/main/factories/controllers/user/add-user-post-controller-factory.ts
+++ src/main/factories/controllers/user/add-user-post-controller-factory.ts
@@ -4,7 +4,7 @@ import { makeAddUserPostValidation } from './add-user-post-validation-factory'
 import { DbAddAccountUserPost } from '../../../../data/usecases/add-user-post/db-add-user-post'
 import { UserPostMongoRepository } from '../../../../infra/db/mongodb/user/user-post-mongo-repository'
 
-export const makeUserPostControllerFactory = (): Controller => {
+export const makeUserPostController = (): Controller => {
   const userPostMongoRepository = new UserPostMongoRepository()
   const dbAddAccountUserPost = new DbAddAccountUserPost(userPostMongoRepository)
   const controller = new AddUserPostController(dbAddAccountUserPost, makeAddUserPostValidation())
--- src/main/routes/user-post-route.ts
+++ src/main/routes/user-post-route.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapter/express/express-router-adapter'
-import { makeUserPostControllerFactory } from '../factories/controllers/user/add-user-post-controller-factory'
+import { makeUserPostController } from '../factories/controllers/user/add-user-post-controller-factory'
 import { makeAllUserPostController } from '../factories/controllers/user/all-user-post-controller-factory'
 
 export default (router: Router): void => {
-  router.post('/user-post', adaptRoute(makeUserPostControllerFactory()))
+  router.post('/user-post', adaptRoute(makeUserPostController()))
   router.get('/user-post', adaptRoute(makeAllUserPostController()))
 }

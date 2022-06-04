1fc8bd1fbba136f506c168d93168be4e97559738
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-03 21:08:27 -0300

    feat: create user-post get router

--- src/main/routes/user-post-route.ts
+++ src/main/routes/user-post-route.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapter/express/express-router-adapter'
 import { makeUserPostControllerFactory } from '../factories/controllers/user/add-user-post-controller-factory'
+import { makeAllUserPostController } from '../factories/controllers/user/all-user-post-controller-factory'
 
 export default (router: Router): void => {
   router.post('/user-post', adaptRoute(makeUserPostControllerFactory()))
+  router.get('/user-post', adaptRoute(makeAllUserPostController()))
 }

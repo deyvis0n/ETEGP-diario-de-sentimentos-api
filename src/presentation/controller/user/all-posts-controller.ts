8b15807b8083b8639e56c259f37b858e020fe312
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-07 17:21:28 -0300

    feat: ensure AllPostsController return 500 if AllPosts throws

--- src/presentation/controller/user/all-posts-controller.ts
+++ src/presentation/controller/user/all-posts-controller.ts
@@ -1,13 +1,17 @@
 import { AllPosts } from '../../../domain/usercase/all-posts'
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
-import { ok } from '../../helper/http/http-helper'
+import { ok, serverError } from '../../helper/http/http-helper'
 
 export class AllPostsController implements Controller {
   constructor (private readonly allPosts: AllPosts) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const postArray = await this.allPosts.findAll()
-    return ok(postArray)
+    try {
+      const postArray = await this.allPosts.findAll()
+      return ok(postArray)
+    } catch (error) {
+      return serverError()
+    }
   }
 }

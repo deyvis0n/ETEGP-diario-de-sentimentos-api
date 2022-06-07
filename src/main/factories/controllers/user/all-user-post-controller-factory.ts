558fb36271bd8873819189fda303c342a4781c89
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-06 22:29:08 -0300

    feat: add accountMongoRepository on makeAllUserPostController

--- src/main/factories/controllers/user/all-user-post-controller-factory.ts
+++ src/main/factories/controllers/user/all-user-post-controller-factory.ts
@@ -3,9 +3,11 @@ import { AllUserPosterController } from '../../../../presentation/controller/use
 import { DbAllUserPost } from '../../../../data/usecases/add-user-post/db-all-user-post'
 import { makeAllUserPostVallidation } from './all-user-post-validation-factory'
 import { UserPostMongoRepository } from '../../../../infra/db/mongodb/user/user-post-mongo-repository'
+import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository'
 
 export const makeAllUserPostController = (): Controller => {
   const userPostMongoRepository = new UserPostMongoRepository()
-  const dbAllUserPost = new DbAllUserPost(userPostMongoRepository)
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbAllUserPost = new DbAllUserPost(userPostMongoRepository, accountMongoRepository)
   return new AllUserPosterController(makeAllUserPostVallidation(), dbAllUserPost)
 }

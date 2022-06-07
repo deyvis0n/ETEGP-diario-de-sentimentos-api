e1cd87d889a3cf4f2f3efc0f4fe97f5df3094d7f
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-07 00:06:05 -0300

    test: ensure DbLoadAccountByToken throws if LoadAccountByIdRepository throws

--- src/data/usecases/authentication/db-load-account-by-token.spec.ts
+++ src/data/usecases/authentication/db-load-account-by-token.spec.ts
@@ -74,10 +74,17 @@ describe('DbLoadAccountByToken', () => {
     expect(account).toEqual(accountToCompare)
   })
 
-  test('Should return null if LoadAccountByIdRepository returnsnull', async () => {
+  test('Should return null if LoadAccountByIdRepository returns null', async () => {
     const { sut, loadAccountByIdRepositoryStub } = makeSut()
     jest.spyOn(loadAccountByIdRepositoryStub, 'loadById').mockReturnValueOnce(null)
     const account = await sut.load('any_token')
     expect(account).toBeNull()
+  })
+
+  test('Should return throws if LoadAccountByIdRepository throws', async () => {
+    const { sut, loadAccountByIdRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByIdRepositoryStub, 'loadById').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.load('any_token')
+    await expect(promise).rejects.toThrow()
   })
 })

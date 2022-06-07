0528f9365d184554fff5cbad2b110f01a964b4a6
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-07 01:31:06 -0300

    feat: add auth on routes

--- src/main/routes/user-post-route.ts
+++ src/main/routes/user-post-route.ts
@@ -2,8 +2,9 @@ import { Router } from 'express'
 import { adaptRoute } from '../adapter/express/express-router-adapter'
 import { makeUserPostController } from '../factories/controllers/user/add-user-post-controller-factory'
 import { makeAllUserPostController } from '../factories/controllers/user/all-user-post-controller-factory'
+import { auth } from '../middlewares/auth'
 
 export default (router: Router): void => {
-  router.post('/user-post', adaptRoute(makeUserPostController()))
-  router.get('/user-post', adaptRoute(makeAllUserPostController()))
+  router.post('/user-post', auth, adaptRoute(makeUserPostController()))
+  router.get('/user-post', auth, adaptRoute(makeAllUserPostController()))
 }

4f8e9f4e06efe62e871e50cc790a31b7ccac8236
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-07 21:55:25 -0300

    feat: ensure UserPostMongoRepository findByUserId returns correct values

--- src/infra/db/mongodb/user/user-post-mongo-repository.ts
+++ src/infra/db/mongodb/user/user-post-mongo-repository.ts
@@ -3,8 +3,9 @@ import { AddUserPostModel } from '../../../../domain/usercase/add-user-post'
 import { FindAllUserPostByUseIdRepository } from '../../../../data/protocols/db/find-all-user-post-by-user-id'
 import { MongoHelper } from '../helper/mongo-helper'
 import { UserPostModel } from '../../../../domain/model/user-post'
+import { FindAllPosts } from '../../../../data/protocols/db/find-all-posts'
 
-export class UserPostMongoRepository implements AddUserPostRepository, FindAllUserPostByUseIdRepository {
+export class UserPostMongoRepository implements AddUserPostRepository, FindAllUserPostByUseIdRepository, FindAllPosts {
   async add (userPost: AddUserPostModel): Promise<void> {
     const userPostCollection = MongoHelper.getCollection('user-posts')
     const userId = MongoHelper.objectId(userPost.userId)
@@ -20,6 +21,16 @@ export class UserPostMongoRepository implements AddUserPostRepository, FindAllUs
     const userPostCollection = MongoHelper.getCollection('user-posts')
     const userIdObject = MongoHelper.objectId(userId)
     const postArray = await userPostCollection.find({ userId: userIdObject }).toArray()
+    const userPostArray = []
+    for (const post of postArray) {
+      userPostArray.push(Object.assign(MongoHelper.postMap(post), { userName: null }))
+    }
+    return userPostArray
+  }
+
+  async findAll (): Promise<UserPostModel[]> {
+    const userPostCollection = MongoHelper.getCollection('user-posts')
+    const postArray = await userPostCollection.find({}).toArray()
     const userPostArray = []
     for (const post of postArray) {
       userPostArray.push(Object.assign(MongoHelper.postMap(post), { userName: null }))

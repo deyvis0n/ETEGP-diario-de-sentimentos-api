dd07fe60a44b39044c1a396f86931095b6079aab
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-07 21:55:51 -0300

    test: ensure UserPostMongoRepository findByUserId returns correct values

--- src/infra/db/mongodb/user/user-post-mongo-repository.spec.ts
+++ src/infra/db/mongodb/user/user-post-mongo-repository.spec.ts
@@ -65,4 +65,43 @@ describe('UserPostMongoRepository', () => {
     expect(userPostArray[1].message).toEqual('any_message_3')
     expect(userPostArray[1].date).toBeTruthy()
   })
+
+  test('Should findByUserId returns correct values', async () => {
+    const sut = new UserPostMongoRepository()
+    const targetId = MongoHelper.objectId('62981df76d3661cf33982509')
+    const noTargetId = MongoHelper.objectId('6297e8eb0bf54e0163efcc5f')
+    await userPostCollection.insertMany([
+      {
+        userId: targetId,
+        message: 'any_message_1',
+        date: new Date(2022, 1, 1)
+      },
+      {
+        userId: noTargetId,
+        message: 'any_message_2',
+        date: new Date(2022, 1, 1)
+      },
+      {
+        userId: targetId,
+        message: 'any_message_3',
+        date: new Date(2022, 1, 1)
+      }
+    ])
+    const userPostArray = await sut.findAll()
+    expect(userPostArray[0].id).toBeTruthy()
+    expect(userPostArray[0].userId).toEqual('62981df76d3661cf33982509')
+    expect(userPostArray[0].userName).toBeNull()
+    expect(userPostArray[0].message).toEqual('any_message_1')
+    expect(userPostArray[0].date).toBeTruthy()
+    expect(userPostArray[1].id).toBeTruthy()
+    expect(userPostArray[1].userId).toEqual('6297e8eb0bf54e0163efcc5f')
+    expect(userPostArray[1].userName).toBeNull()
+    expect(userPostArray[1].message).toEqual('any_message_2')
+    expect(userPostArray[2].date).toBeTruthy()
+    expect(userPostArray[2].id).toBeTruthy()
+    expect(userPostArray[2].userId).toEqual('62981df76d3661cf33982509')
+    expect(userPostArray[2].userName).toBeNull()
+    expect(userPostArray[2].message).toEqual('any_message_3')
+    expect(userPostArray[2].date).toBeTruthy()
+  })
 })

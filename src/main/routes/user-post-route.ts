944a950e886f841f4fc16b7bf9916954e0abd334
deyvis0n <deyvisonsferreira@outlook.com>
2022-06-07 22:09:47 -0300

    feat: create all-post router

--- src/main/routes/user-post-route.ts
+++ src/main/routes/user-post-route.ts
@@ -2,9 +2,11 @@ import { Router } from 'express'
 import { adaptRoute } from '../adapter/express/express-router-adapter'
 import { makeUserPostController } from '../factories/controllers/user/add-user-post-controller-factory'
 import { makeAllUserPostController } from '../factories/controllers/user/all-user-post-controller-factory'
+import { makeAllPostsController } from '../factories/controllers/user/all-posts-controller-factory'
 import { auth } from '../middlewares/auth'
 
 export default (router: Router): void => {
   router.post('/user-post', auth, adaptRoute(makeUserPostController()))
   router.get('/user-post', auth, adaptRoute(makeAllUserPostController()))
+  router.get('/all-post', auth, adaptRoute(makeAllPostsController()))
 }
